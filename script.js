5435f5f55a7fa7e7beca3ded0c4dd02594232b4c
NicholasdeOliveiraSica <nick.sica16@gmail.com>
2024-08-07 13:09:24 -0300

    terminei o dolar, refiz parte do codigo js

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-let seletorUm = document.querySelector(".select1")
-let seletorDois = document.querySelector(".select2")
-let caixaValor = document.querySelector(".inputValor")
-let botao = document.querySelector(".botao")
-let bandeira1 = document.querySelector(".bandeira1")
-let moeda1 = document.querySelector(".moeda1")
-let valor1 = document.querySelector(".valor1")
-let bandeira2 = document.querySelector(".bandeira2")
-let moeda2 = document.querySelector(".moeda2")
-let valor2 = document.querySelector(".valor2")
-let valorFinal1 = 0.00
-let valorFinal2 = 0.00
+const seletorUm = document.querySelector(".select1")
+const seletorDois = document.querySelector(".select2")
 
+const inputVar = document.querySelector(".inputValor")
+
+const botao = document.querySelector(".botao")
+const moeda1 = document.querySelector(".moeda1")
+const moeda2 = document.querySelector(".moeda2")
+const bandeira1 = document.querySelector(".bandeira1")
+const bandeira2 = document.querySelector(".bandeira2")
+const valor1 = document.querySelector(".valor1")
+const valor2 = document.querySelector(".valor2")
+
+let valorDolar = 5.66
+let valorEuro = 6.23
 
 
 botao.addEventListener("click", converter)
+inputVar.addEventListener("change", function(event) {
+})
 
 function converter() {
-    valorFinal1 = caixaValor.value
-    valorFinal2 = (caixaValor.value / 5.66)
-}
+    caixaValor = inputVar.value
 
-for ( x = 1 ; xz = 10; xzy = 10){
-valor1.textContent = "R$ " + valorFinal1
-valor2.textContent = "US$ " + valorFinal2
+    valor1.innerHTML = new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    }).format(caixaValor)
+
+    if (seletorDois.value = 'dolar') {
+        valor2.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(caixaValor/valorDolar)
+    }
 }
